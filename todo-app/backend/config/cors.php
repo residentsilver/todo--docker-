404351434e37c0b8e3ba438449e89375862370ba
residentsilver <testoftest.fornara@gmail.com>
2025-07-28 18:25:25 +0900

    デプロイのため修正

--- todo-app/backend/config/cors.php
+++ todo-app/backend/config/cors.php
@@ -2,7 +2,11 @@
     return [
         'paths' => ['api/*', 'sanctum/csrf-cookie'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'], // 開発環境用
+        'allowed_origins' => [
+            'http://localhost:3000', 
+            'http://127.0.0.1:3000',
+            'https://todo.sumaho-clinic.com'  // 本番環境用ドメインを追加
+        ],
         'allowed_origins_patterns' => [],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
