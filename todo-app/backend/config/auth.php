8194a1240301df5e4ce0a5eb5640939c8d24bf16
residentsilver <testoftest.fornara@gmail.com>
2025-07-29 00:14:42 +0900

    認証の調整

--- todo-app/backend/config/auth.php
+++ todo-app/backend/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'sanctum'), // webからsanctumに変更
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
